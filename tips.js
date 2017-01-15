420e57dcc660eb87122df1ebb47cff242a2f7164
Mark Canlas <github@htmlism.com>
2017-01-14 21:01:59 -0500

    added tips

---
+++ tips.js
@@ -0,0 +1,97 @@
+var tips = [
+  {
+    title: "Active listening",
+    description: "For support and to inform callbacks/world-building",
+    type: "improv"
+  },
+  {
+    title: "Matching",
+    description: "An easy way to be supportive. And buys time",
+    type: "improv"
+  },
+  {
+    title: "Initiate strongly",
+    description: "Open scenes with detail or in a situation mid-flight",
+    type: "improv"
+  },
+  {
+    title: "Cast small lines",
+    description: "Gifts should be small and open to interpretation. Lengthy, complex thoughts can be difficult to navigate",
+    type: "improv"
+  },
+  {
+    title: "Maintain eye contact",
+    description: "For non-verbal communication",
+    type: "improv"
+  },
+  {
+    title: "Use silence",
+    description: "Not every space needs to be filled with noise",
+    type: "improv"
+  },
+  {
+    title: "Pull the trigger",
+    description: 'Subtly is not your friend. Openly state your relationship with "you make me feel <blank>"',
+    type: "improv"
+  },
+  {
+    title: "Come loaded with an emotion",
+    description: "If you come loaded, reactions will be simpler and more informed",
+    type: "improv"
+  },
+  {
+    title: "Physicality is real",
+    description: 'Know what an "improv phone" and an "improv gun" look like',
+    type: "improv"
+  },
+  {
+    title: "Detail is zooming out",
+    description: 'Improv is backing up. Backing up is discovery. Improv is who, what, where -- Keegan-Michael Key',
+    type: "untested"
+  },
+  {
+    title: "Stage picture",
+    description: "Use an interesting shape. Don't just stay as a back line.",
+    type: "musical"
+  },
+  {
+    title: "Shorter is better",
+    description: 'A short chorus or tag is easier to remember for both you and your teammates. And leaves more room for freestyle.',
+    type: "musical"
+  },
+  {
+    title: "Get away from the plot",
+    description: 'Motivation and relationships are more important than plot elements.',
+    type: "musical"
+  },
+  {
+    title: "Marry, kill, thwart",
+    description: 'Villain resolutions. Join the protagonist, get killed, or have your philosophy thwarted',
+    type: "musical"
+  },
+  {
+    title: "Every character has a want",
+    description: 'Even charm characters',
+    type: "musical"
+  },
+  {
+    title: "Find what is wrong with life",
+    description: 'For protagonist scenes',
+    type: "musical"
+  },
+  {
+    title: "Don't repeat scene details in song",
+    description: 'Wasted opportunity for more detail',
+    type: "musical"
+  },
+  {
+    title: "Personal experience informs",
+    description: "It's easier to sing about personal experiences in great detail",
+    type: "musical"
+  },
+  {
+    title: "Leave on verses",
+    description: "Draws focus to verse. Enables walk-ons",
+    type: "musical"
+  },
+]
